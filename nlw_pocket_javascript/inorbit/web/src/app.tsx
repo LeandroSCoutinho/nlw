005503fa299476525d9c75662a9301733cb301be
Leandro Coutinho <franciscoleandrosilvacoutinho@gmail.com>
2024-09-14 18:15:59 -0300

    ui components

--- nlw_pocket_javascript/inorbit/web/src/app.tsx
+++ nlw_pocket_javascript/inorbit/web/src/app.tsx
@@ -2,6 +2,7 @@ import { Plus } from 'lucide-react'
 
 import logo from './assets/logo-in-orbit.svg'
 import letsStart from './assets/lets-start-illustration.svg'
+import { Button } from './components/ui/button'
 
 export function App() {
   return (
@@ -9,16 +10,14 @@ export function App() {
       <img src={logo} alt="in.orbit" />
       <img src={letsStart} alt="in.orbit" />
       <p className="text-zinc-300 leading-relaxed max-w-80 text-center">
-        Você ainda não cadastrou nenhuma meta, que tal cadastrar um agora mesmo?
+        Você ainda não cadastrou nenhuma meta, que tal cadastrar uma agora
+        mesmo?
       </p>
 
-      <button
-        type="button"
-        className="px-4 py-2.5 rounded-lg bg-violet-500 text-violet-50 flex items-center gap-2 text-sm font-medium tracking-tight hover:bg-violet-600"
-      >
+      <Button>
         <Plus className="size-4" />
         Cadastrar meta
-      </button>
+      </Button>
     </div>
   )
 }
