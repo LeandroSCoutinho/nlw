cf916be0016e5ecefdeb746a281148b3f933eb82
Leandro Coutinho <franciscoleandrosilvacoutinho@gmail.com>
2024-09-13 22:32:15 -0300

    create raute get-week-summary

--- nlw_pocket_javascript/inorbit/server/src/http/server.ts
+++ nlw_pocket_javascript/inorbit/server/src/http/server.ts
@@ -8,8 +8,14 @@ import {
 import { createGoalRoute } from "./routes/create-gola";
 import { createCompletionRoute } from "./routes/create-completion";
 import { getPendingGoalRoute } from "./routes/get-pending-goal";
+import { getWeekSummaryRoute } from "./routes/get-weeek-summary";
+import fastifyCors from "@fastify/cors";
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
+
+app.register(fastifyCors, {
+    origin: '*',
+})
 
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
@@ -17,6 +23,7 @@ app.setSerializerCompiler(serializerCompiler);
 app.register(createGoalRoute)
 app.register(createCompletionRoute)
 app.register(getPendingGoalRoute)
+app.register(getWeekSummaryRoute)
 
 app.listen({
     port: 3333,
