c3d6db643d311ac89e091b6845d44c15a259a73c
Leandro Coutinho <franciscoleandrosilvacoutinho@gmail.com>
2024-09-23 10:00:00 -0300

    method http getSummary with tanstack-query

--- nlw_pocket_javascript/inorbit/web/src/app.tsx
+++ nlw_pocket_javascript/inorbit/web/src/app.tsx
@@ -1,13 +1,24 @@
 import { Dialog } from './components/ui/dialog'
 import { CreateGoal } from './components/create-goal'
 import { Summary } from './components/sumary'
-// import { EmptyGoals } from './components/empty-goals'
+import { useQuery } from '@tanstack/react-query'
+import { EmptyGoals } from './components/empty-goals'
+import { getSummary } from './http/get-summary'
+
+
+
 
 export function App() {
+  const { data } = useQuery({
+    queryKey: ['summary'],
+    queryFn: getSummary
+  })
+
   return (
     <Dialog>
-      {/* <EmptyGoals /> */}
-      <Summary />
+      {data?.total && data.total > 0 ? <Summary /> : <EmptyGoals />}
+     
+      
       <CreateGoal />
     </Dialog>
   )
